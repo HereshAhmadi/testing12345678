09a1781cf9bec6b6c7a4aa441da5948e48300f3b
809882 <809882@SAIT225648IT.ACDM.DS.SAIT.CA>
2021-06-22 12:36:29 -0600

    product update

--- src/main/java/com/deerpointgroup/deerpointliquorstore/product/ProductController.java
+++ src/main/java/com/deerpointgroup/deerpointliquorstore/product/ProductController.java
@@ -1,6 +1,7 @@
 package com.deerpointgroup.deerpointliquorstore.product;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,67 +9,78 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
-    
 
     private final ProductService productService;
-    
-    
+
     @Autowired
-    public ProductController(ProductService productService){
+    public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
     @GetMapping
-    public List<Product> getProducts(){
+    public List<Product> getProducts() {
         return productService.getAllProducts();
     }
+
+    @RequestMapping(value = "/mostsold", method = RequestMethod.GET)
+    public List<Product> getMostSoldItems() {
+        return productService.getMostSoldProducts();
+    }
     
+//    @RequestMapping(value = "/mostsold", method = RequestMethod.GET)
+//    public List<Product> getMostSoldItems() {
+//        return productService.getMostSoldProducts();
+//    }
+
+//    @RequestMapping(value = "/mostsold", method = RequestMethod.GET)
+//    public List<Product> getMostSoldItems() {
+//        return productService.getMostSoldProducts();
+//    }
     
+
     @PostMapping
-    public String newProduct( @RequestParam(required = true) String productName,
-                                    @RequestParam(required = true) String productDescription,
-                                    @RequestParam(required = true) int productQuantity,
-                                    @RequestParam(required = true) double productPrice){
-        
-        if(productName != null && productName.length() > 0 ){
-            try{
-               productService.addNewProduct(new Product(productName, productDescription, productQuantity, productPrice)); 
-               return "Product Added";
-            }catch(Exception e){
+    public String newProduct(@RequestParam(required = true) String productName,
+            @RequestParam(required = true) String productDescription,
+            @RequestParam(required = true) int productQuantity,
+            @RequestParam(required = true) double productPrice,
+            @RequestParam(required = true) double productDiscount) {
+
+        if (productName != null && productName.length() > 0) {
+            try {
+                productService.addNewProduct(new Product(productName, productDescription, productQuantity, productPrice));
+                return "Product Added";
+            } catch (Exception e) {
                 return "Product ID already used";
             }
-            
-        }else{
+
+        } else {
             return "Passwords do not match";
         }
-        
+
     }
-    
+
     //Deleting a product
     @DeleteMapping(path = "{productID}")
-    public void deleteProduct(@PathVariable("productID") int productID ){
+    public void deleteProduct(@PathVariable("productID") int productID) {
         productService.deleteProduct(productID);
     }
-    
+
     //for updating
     @PutMapping(path = "{productID}")
     public void updateProduct(@PathVariable("productID") int productID,
-                              @RequestParam(required = false) String productName,
-                              @RequestParam(required = false) String productDescription,
-                              @RequestParam(required = false) int productQuantity,
-                              @RequestParam(required = false) double productPrice){
-        productService.updateProduct(productID, productName, productDescription, productQuantity, productPrice);
+            @RequestParam(required = false) String productName,
+            @RequestParam(required = false) String productDescription,
+            @RequestParam(required = false) int productQuantity,
+            @RequestParam(required = false) double productPrice,
+            @RequestParam(required = false) double productDiscount) {
+        productService.updateProduct(productID, productName, productDescription, productQuantity, productPrice, productDiscount);
     }
-    
-//    public void getAllProducts(){
-//        productService.getAllProducts();
-//        
-//    }
-    
+
 }
