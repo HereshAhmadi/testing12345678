d2ad48a509e2737d928b0fb5b30ccd0d3e085076
699785 <699785@SAIT225170IT.ACDM.DS.SAIT.CA>
2021-06-29 10:30:26 -0600

    Changed user to customer Changed user to customer

--- src/main/java/com/deerpointgroup/deerpointliquorstore/product/ProductRepository.java
+++ src/main/java/com/deerpointgroup/deerpointliquorstore/product/ProductRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 interface ProductRepository extends JpaRepository<Product, Long>{
     @Query("SELECT s FROM Product s WHERE s.productName = ?1")
     Optional<Product> findProductByName(String name);
+    
     @Query(value = "SELECT * FROM Product ORDER BY quantity_sold DESC fetch first 3 rows only ", nativeQuery = true)
     List<Product> findMostSoldProducts();
 }
