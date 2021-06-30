d4b608d1b6affefb8c34b999c786e4c4dd5fe516
699785 <699785@SAIT225170IT.ACDM.DS.SAIT.CA>
2021-06-30 10:20:49 -0600

    added search search

--- src/main/java/com/deerpointgroup/deerpointliquorstore/product/ProductService.java
+++ src/main/java/com/deerpointgroup/deerpointliquorstore/product/ProductService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Service
 public class ProductService {
 
@@ -21,60 +20,56 @@ public class ProductService {
     public List<Product> getProducts() {
         return productRepository.findAll();
     }
-    
-    
-    public void addNewProduct(Product product){
-       Optional<Product> productOptional = productRepository.findProductByName(product.getProductName());
-       
-       if(productOptional.isPresent()){
-           throw new IllegalStateException("Product already in database");
-       }else{
-           productRepository.save(product);
-       }
-     }
-    
-    
-    
-    public void deleteProduct(long productId){
-        boolean exists = productRepository.existsById(productId);
-        
-       if(!exists){
-           throw new IllegalStateException("Product with id" + productId + " does not exist");
-       }
-       
-       productRepository.deleteById(productId);
+
+    public void addNewProduct(Product product) {
+        Optional<Product> productOptional = productRepository.findProductByName(product.getProductName());
+
+        if (productOptional.isPresent()) {
+            throw new IllegalStateException("Product already in database");
+        } else {
+            productRepository.save(product);
+        }
     }
-    
-    
-    @Transactional
-    public void updateProduct(long productId, String name, String productDescription, int productQuantity, double productPrice, double productDiscount){
+
+    public void deleteProduct(long productId) {
         boolean exists = productRepository.existsById(productId);
-        
-        if(!exists){
-            throw new IllegalStateException("Product with " + productId +" does not exists");
+
+        if (!exists) {
+            throw new IllegalStateException("Product with id" + productId + " does not exist");
         }
-        
+
+        productRepository.deleteById(productId);
+    }
+
+    @Transactional
+    public void updateProduct(long productId, String name, String productDescription, int productQuantity, double productPrice, double productDiscount) {
+        boolean exists = productRepository.existsById(productId);
+
+        if (!exists) {
+            throw new IllegalStateException("Product with " + productId + " does not exists");
+        }
+
         Product product = productRepository.getById(productId);
-        
-        if(name != null & name.equals(" ") && !Objects.equals(product.getProductName(), name)){
-           product.setProductName(name);
+
+        if (name != null & name.equals(" ") && !Objects.equals(product.getProductName(), name)) {
+            product.setProductName(name);
         }
-        
-        if(name != null & name.equals(" ") && !Objects.equals(product.getProductName(), name)){
-            
+
+        if (name != null & name.equals(" ") && !Objects.equals(product.getProductName(), name)) {
+
             Optional<Product> productOptional = productRepository.findProductByName(name);
-            
-                if(productOptional.isPresent()){
-                    throw new IllegalStateException("Product already exists in the database");
-                }
+
+            if (productOptional.isPresent()) {
+                throw new IllegalStateException("Product already exists in the database");
+            }
             product.setProductName(name);
         }
     }
-    
+
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
-    
+
     public List<Product> getMostSoldProducts() {
         return productRepository.findMostSoldProducts();
     }
@@ -82,8 +77,13 @@ public class ProductService {
     public List<Product> getOnSaleProducts() {
         return productRepository.findOnSaleProducts();
     }
-    
+
     public List<Product> getFeaturedProducts() {
         return productRepository.findFeaturedProducts();
     }
+
+    public List<Product> searchProducts(String name) {
+        return productRepository.searchProducts(name);
+    }
+
 }
